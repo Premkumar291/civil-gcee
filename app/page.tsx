8d9ef37da8122fceecadd3ce2e2cd10db0aff2b3
Premkumar291 <premkumar.29105@gmail.com>
2026-03-06 21:51:26 +0530

    Initial Commit

--- app/page.tsx
+++ app/page.tsx
@@ -9,8 +9,8 @@ export default function Home() {
           src="/next.svg"
           alt="Next.js logo"
           width={100}
-          height={20}
-          priority
+          height={30}
+          priority 
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
